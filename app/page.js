c9ad4170e809ff2449db84e07cf2f6c3c355e1ad
Krrish Goswami <goswamikrrish67@gmail.com>
2025-03-29 14:05:53 +0530

    responsivness added

--- app/page.js
+++ app/page.js
@@ -2,30 +2,24 @@
 import Link from "next/link";
 import Image from "next/image";
 import localFont from "next/font/local";
+
 const dancingscript = localFont({
   src: "../fonts/DancingScript-VariableFont_wght.ttf",
   variable: "--font-dancingscript",
   weight: "100 900",
 });
+
 export default function Home() {
   return (
-    <div className="flex relative">
-      <div className="flex absolute">
+    <div className="flex relative w-full">
       <Image
-        className="mix-blend-darken"
-        src={"/bakery.png"}
-        alt="An image of vector"
-        width={500}
-        height={500}
-      />
-      <div className="flex relative top-110 right-105 text-2xl  font-black"><h1 className={`${dancingscript.className}`}>Freshly baked happiness in every bite!</h1></div>
-      </div>
-      <Image
-        className="mix-blend-darken"
-        src={"/home.jpg"}
+        className="mix-blend-darken object-cover"
+        src={"/home.png"}
         alt="An image of vector"
         width={1500}
         height={800}
+        layout="responsive"
+        sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 1500px"
       />
     </div>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div className="flex justify-center ">
-      <nav className="flex justify-center ">
-        <ul className="flex gap-5 p-5 mb-2 mt-5 text-xl font-bold">
+    <div className="flex justify-center w-full">
+      <nav className="flex justify-center w-full">
+        <ul className="flex flex-wrap gap-5 p-5 mb-2 mt-5 text-lg md:text-xl font-bold justify-center">
           <Link href="/">
-            <li>Home</li>
+            <li className="cursor-pointer hover:text-gray-500">Home</li>
           </Link>
           <Link href="/order">
-            <li>Order</li>
+            <li className="cursor-pointer hover:text-gray-500">Explore</li>
           </Link>
           <Link href="/about">
-            <li>About</li>
+            <li className="cursor-pointer hover:text-gray-500">About</li>
           </Link>
           <Link href="/contact">
-            <li>Contact us</li>
+            <li className="cursor-pointer hover:text-gray-500">Contact us</li>
           </Link>
         </ul>
       </nav>
